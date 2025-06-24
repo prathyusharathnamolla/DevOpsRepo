cb51de6d07976e484451d6070607c228604edbe8
prathyusharathnamolla <78398719+prathyusharathnamolla@users.noreply.github.com>
2025-06-23 21:16:10 -0400

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,10 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello World!");
+        // Keep the app running indefinitely
+        Thread.sleep(Long.MAX_VALUE);
     }
 }
