cceac3a1c8ccd9040fbfcc04820cd340cc304910
prathyusharathnamolla <78398719+prathyusharathnamolla@users.noreply.github.com>
2025-06-23 21:35:02 -0400

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,3 +1,8 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
