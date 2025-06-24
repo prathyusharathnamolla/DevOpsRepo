b455e18353af7dd516156e944f26cfbb1f61eda4
prathyusharathnamolla <78398719+prathyusharathnamolla@users.noreply.github.com>
2025-06-23 21:21:48 -0400

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,6 @@
-package com.example;
-
-public class App 
-{
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello World!");
-        // Keep the app running indefinitely
-        Thread.sleep(Long.MAX_VALUE);
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
     }
 }
